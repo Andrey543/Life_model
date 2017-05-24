264129c5311e6491b3557a32c24dffbfe8a6e0f7
Andrey543 <andrey_rybakov_97@mail.ru>
2017-05-25 00:44:10 +0300

    Update Wolf, Creator, Create Island
    Создал класс "остров", написал половину класса "волк",  наполовину написал функцию редактора островов

--- main.cpp
+++ main.cpp
@@ -1,97 +1,109 @@
 #include <SFML\Graphics.hpp>
-#include<Windows.h>
+#include <Windows.h>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
 
 
-/*
+
 
 class Game_Unit
 {
-protected:
-
+public:
+	int island; // на каком острове находится
 	sf::Time age; // возраст
 	int x;     // координата x
 	int y;	   // координата y
 	sf::Texture image; // Изображение
 	sf::Sprite view;   // Изображение
 
-public:
 
-	Game_Unit(int age_new, int x_new, int y_new, std::string name_of_image_new)
-	{
-		//TODO
-	}
 
-	void setAge(int age_new)
+	Game_Unit(sf::Time age_new, int x_new, int y_new, std::string name_of_image_new, int isl)
 	{
-		//TODO
+		age = age_new;
+		x = x_new;
+		y = y_new;
+		image.loadFromFile(name_of_image_new);
+		view.setTexture(image);
+		view.setPosition(x, y);
+		island = isl;
 	}
 
 	void setPosition(int x_new, int y_new)
 	{
-		//TODO
+		view.setPosition(x, y);
 	}
 
 	void setImage(std::string name_of_image_new)
 	{
-		//TODO
+		image.loadFromFile(name_of_image_new);
+		view.setTexture(image);
 	}
 
-	int getXPos()
-	{
-		return x;
-	}
-
-	int getYPos()
-	{
-		return y;
-	}
-
-	int getAge()
-	{
-		//TODO
-	}
-	//TODO
 	~Game_Unit() {}
 
 };
 
 class Wolf : public Game_Unit
 {
-private:
-
-	int gender;
-	int fullness;
-
 public:
 
-	Wolf(int age, int x, int y, std::string name_of_image, int gender_new, int fullness_new) : Game_Unit(age, x, y, name_of_image)
+	int gender;  // Пол: 1 - мальчик, 2 - девочка
+	int fullness; // Сытость
+
+
+
+	Wolf(sf::Time age, int x, int y, std::string name_of_image, int gender_new, int fullness_new, int isl) : Game_Unit(age, x, y, name_of_image,isl)
 	{
-		//TODO
+		gender = gender_new;
+		fullness = fullness_new;
 	}
 
-	void updateFullness(int meal)
-	{
-		//TODO
-	}
-
-	int getFullness()
-	{
-		//TODO
-	}
 	
-	int ChoiceDirection()
-	{
-		//TODO
-	}
-
 	void Move(int direction)
 	{
+		srand(time(0));
+		int c = rand() % 4;
+		if (c = 0)
+		{
+			if (x > 0)
+				x = x - 25;
+		}
+		if (c = 1)
+		{
+			if (y > 0)
+				y = y - 25;
+		}
+		if (c = 2)
+		{
+			if (x < 1275)
+				x = x + 25;
+		}
+		if (c = 3)
+		{
+			if (y < 775)
+				y = y + 25;
+		}
+		view.setPosition(x, y);
+	}
+
+	void TryToEat()
+	{
 		//TODO
 	}
 
+	void TryToDeath()
+	{
+		//TODO
+	}
 
+	void TryToMate()
+	{
+		//TODO
+	}
 
 	//TODO
 };
@@ -105,7 +117,7 @@ private:
 
 public:
 
-	Hare(int age, int x, int y, std::string name_of_image, int gender_new, int fullness_new) : Game_Unit(age, x, y, name_of_image)
+	Hare(sf::Time age, int x, int y, std::string name_of_image, int gender_new, int fullness_new, int isl) : Game_Unit(age, x, y, name_of_image, isl)
 	{
 		//TODO
 	}
@@ -138,7 +150,7 @@ class Grass : public Game_Unit
 {
 public:
 
-	Grass(int age, int x, int y, std::string name_of_image, int fullness_new) : Game_Unit(age, x, y, name_of_image)
+	Grass(sf::Time age, int x, int y, std::string name_of_image, int fullness_new, int isl) : Game_Unit(age, x, y, name_of_image, isl)
 	{
 		//TODO
 	}
@@ -156,7 +168,79 @@ public:
 };
 
 
-*/
+
+
+class Island
+{
+private:
+	
+	int number;
+	int x;
+	int y;
+	sf::RectangleShape island;
+	int amount_of_wolfs_male;
+	int amount_of_wolfs_female;
+	int amount_of_hares_male;
+	int amount_of_hares_female;
+
+public:
+
+	Island(int num, int x_i, int y_i, int male_wolfs, int female_wolfs, int male_hares, int female_hares)
+	{
+		number = num;
+		x = x_i;
+		y = y_i;
+		island.setFillColor(sf::Color(255, 161, 84));
+		amount_of_wolfs_male = male_wolfs;
+		amount_of_wolfs_female = female_wolfs;
+		amount_of_hares_male = male_hares;
+		amount_of_hares_female = female_hares;
+	}
+
+	int getAmountOfMaleWolfs()
+	{
+		return amount_of_wolfs_male;
+	}
+
+	int getAmountOfFemaleWolfs()
+	{
+		return amount_of_wolfs_female;
+	}
+
+	int getAmountOfMaleHares()
+	{
+		return amount_of_hares_male;
+	}
+
+	int getAmountOfFemaleHares()
+	{
+		return amount_of_hares_female;
+	}
+
+
+	void setAmountOfMaleWolfs(int number)
+	{
+		amount_of_wolfs_male = number;
+	}
+
+	void setAmountOfFemaleWolfs(int number)
+	{
+		amount_of_wolfs_female = number;
+	}
+
+	void setAmountOfMaleHares(int number)
+	{
+		amount_of_hares_male = number;
+	}
+
+	void setAmountOfFemaleHares(int number)
+	{
+		amount_of_hares_female = number;
+	}
+};
+
+
+
 int menu(sf::RenderWindow * window)
 {
 	sf::Texture background_image; // Картинка, для фона меню
@@ -268,11 +352,96 @@ int menu(sf::RenderWindow * window)
 	}
 }
 
-int creator(sf::RenderWindow * window)
+void creator(sf::RenderWindow * window)
 {
-	//TODO
 
-	return 0;
+	sf::Texture ocean_texture;
+	ocean_texture.loadFromFile("ocean_color_1300_800.png");
+	sf::Sprite ocean;
+	ocean.setTexture(ocean_texture);
+
+	sf::Text back;
+	sf::Font font;
+	font.loadFromFile("Corner-Regular.otf");
+	back.setFont(font);
+	back.setCharacterSize(40);
+	back.setString("BACK");
+	back.setColor(sf::Color::Black);
+	back.setStyle(sf::Text::Bold);
+	back.setPosition(1200, 5);
+	(*window).clear();
+	(*window).draw(ocean);
+	(*window).draw(back);	
+	(*window).display();
+
+	int x, y; // координаты места под остров
+	x = 0;
+	y = 0;
+	sf::RectangleShape candidat(sf::Vector2f(25, 25));
+	candidat.setFillColor(sf::Color(25,40,255));
+	candidat.setPosition(x, y);
+	int need_to_go_back = 0;
+	while ((*window).isOpen())
+	{
+
+		sf::Event event;
+		while ((*window).pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				(*window).close();
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Return)
+			{
+				if (need_to_go_back == 1)
+					return;
+				if (need_to_go_back == 0)
+				{
+					need_to_go_back++;
+					back.setColor(sf::Color::Yellow);
+				}
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Up)
+			{
+				if (y > 0)
+				{
+					y = y - 25;
+					candidat.setPosition(x, y);
+				}
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Down)
+			{
+				if (y < 775)
+				{
+					y = y + 25;
+					candidat.setPosition(x, y);
+				}
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right)
+			{
+				if (x < 1275)
+				{
+					x = x + 25;
+					candidat.setPosition(x, y);
+				}
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left)
+			{
+				if (x > 0)
+				{
+					x = x - 25;
+					candidat.setPosition(x, y);
+				}
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left)
+			{
+
+			}
+			(*window).clear();			
+			(*window).draw(ocean);
+			(*window).draw(candidat);
+			(*window).draw(back);
+			(*window).display();
+		}
+	}
 }
 
 int ocean_game(sf::RenderWindow * window)
@@ -298,6 +467,8 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(x_screen, y_screen), "Game_of_Life"); // Создаем окно, в котором все будет происходить
 	window.setPosition(sf::Vector2i(0, 0)); // Помещаем окно в левый верхний угол
 	window.setVerticalSyncEnabled(true); // Включаем вертикальную синхронизацию
+
+
 	
 	while (window.isOpen())
 	{	
