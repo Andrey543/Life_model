5cb3921c24d902035ce0c2ccd66b4bfd3adc8e45
MargoGV <MV.Gubina@yandex.ru>
2017-05-24 23:16:06 +0300

    Написала int menu()

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 
-
+/*
 
 class Game_Unit
 {
@@ -156,32 +156,144 @@ public:
 };
 
 
-
-int menu()
+*/
+int menu(sf::RenderWindow * window)
 {
-	//TODO
+	sf::Texture background_image; // Картинка, для фона меню
+	background_image.loadFromFile("background_1300_800.png"); // Подгружаем картинку
+	sf::Sprite background; // Элемент фона
+	background.setTexture(background_image);
+	(*window).clear(sf::Color::Green);
+	sf::Text start;
+	sf::Text field_editor;
+	sf::Text back;
+	sf::Text exit;
+
+	sf::Font font;
+	font.loadFromFile("Corner-Regular.otf");
+
+	start.setFont(font);
+	field_editor.setFont(font);
+	exit.setFont(font);
+
+	start.setString("START");
+	field_editor.setString("FIELD EDITOR");
+	exit.setString("EXIT");
+
+	start.setColor(sf::Color::Black);
+	field_editor.setColor(sf::Color::Black);
+	exit.setColor(sf::Color::Black);
+
+	start.setCharacterSize(40);
+	field_editor.setCharacterSize(40);
+	exit.setCharacterSize(40);
+
+	start.setStyle(sf::Text::Bold);
+	field_editor.setStyle(sf::Text::Bold);
+	exit.setStyle(sf::Text::Bold);
+
+	start.setPosition(100, 100);
+	field_editor.setPosition(100, 150);
+	exit.setPosition(100, 200);
+
+	
+	(*window).draw(background);
+	(*window).draw(start);
+	(*window).draw(field_editor);
+	(*window).draw(exit);
+	(*window).display();
+
+	int counter_of_point = 0;
+	while ((*window).isOpen())
+	{
+
+		sf::Event event;
+		while ((*window).pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				(*window).close();
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Return)
+			{
+				if (counter_of_point == 1)
+					return 3;
+				if (counter_of_point == 2)
+					return 2;
+				if (counter_of_point == 3)
+					(*window).close();
+
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Up)
+			{
+				counter_of_point--;
+				if (counter_of_point <= 0)
+					counter_of_point = 3;
+
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Down)
+			{
+				counter_of_point++;
+				counter_of_point = counter_of_point % 3;
+				if (counter_of_point == 0)
+					counter_of_point = 3;
+
+			}
+			if (counter_of_point == 1)
+			{
+				start.setColor(sf::Color::Yellow);
+				field_editor.setColor(sf::Color::Black);
+				exit.setColor(sf::Color::Black);
+			}
+				
+			if (counter_of_point == 2)
+			{
+				start.setColor(sf::Color::Black);
+				field_editor.setColor(sf::Color::Yellow);
+				exit.setColor(sf::Color::Black);
+			}
+				
+			if (counter_of_point == 3)
+			{
+				start.setColor(sf::Color::Black);
+				field_editor.setColor(sf::Color::Black);
+				exit.setColor(sf::Color::Yellow);
+			}
+		
+				
+			(*window).draw(background);
+			(*window).draw(start);
+			(*window).draw(field_editor);
+			(*window).draw(exit);
+			(*window).display();
+		}
+	}
 }
 
-int creator()
+int creator(sf::RenderWindow * window)
 {
 	//TODO
+
+	return 0;
 }
 
-int ocean_game()
+int ocean_game(sf::RenderWindow * window)
 {
 	//TODO
+
+	return 0;
 }
 
-int island_game(int number_of_island)
+int island_game(int number_of_island, sf::RenderWindow * window)
 {
 	//TODO
+
+	return 0;
 }
 
 int main()
 {
 	int mode = 1; // Где находится пользователь: 1 - меню, 2 - редактор карты, 3 - океан, 4 - остров
 	int number_of_island = 0; // Идентификатор острова, на который хочет отправиться игрок
-	int x_screen = 1500;  // Размер окна по горизонтали.
+	int x_screen = 1300;  // Размер окна по горизонтали.
 	int y_screen = 800;   // Размер окна по вертикали.
 	sf::RenderWindow window(sf::VideoMode(x_screen, y_screen), "Game_of_Life"); // Создаем окно, в котором все будет происходить
 	window.setPosition(sf::Vector2i(0, 0)); // Помещаем окно в левый верхний угол
@@ -190,19 +302,22 @@ int main()
 	while (window.isOpen())
 	{	
 		if (mode == 1)
-			mode = menu();
+			mode = menu(&window);
 
-		if (mode == 2);
+		if (mode == 2)
 		{
-			creator();
-			mode = menu(); 
+			creator(&window);
+			mode = menu(&window);
 		}
 
 		if (mode == 3)
 		{
-			number_of_island = ocean_game();
-			island_game(number_of_island);
+			number_of_island = ocean_game(&window);
+			island_game(number_of_island, &window);
 		}
+		
+			
+		
 		
 	}
 	
